a4cc755211bd0ae606c8a92f574f3ec39df07ade
Kiss <kissmarcell1@students.jedlik.eu>
2025-04-30 08:32:30 +0200

    4

--- scripts/levels.js
+++ scripts/levels.js
@@ -54,7 +54,7 @@ level4.push(new Obstacle(1150, 405, "img/kek.png", 0.185))
 level4.push(new Obstacle(1150, 490, "img/sarga.png", 0.185))
 level4.push(new Obstacle(1150, 680, "img/kek.png", 0.185))
 
-level4.push(new Obstacle(990, 592, "img/rendor.png", 0.6))
+level4.push(new Obstacle(990, 572, "img/rendor.png", 0.6))
 level4.push(new Obstacle(1000, 315, "img/kek.png", 0.185))
 level4.push(new Obstacle(1000, 230, "img/sarga.png", 0.185))
 level4.push(new Obstacle(1000, 405, "img/sarga.png", 0.185))
@@ -71,15 +71,15 @@ level4.push(new Obstacle(770, 315, "img/kek.png", 0.185))
 level4.push(new Obstacle(770, 230, "img/sarga.png", 0.185))
 level4.push(new Obstacle(770, 405, "img/sarga.png", 0.185))
 level4.push(new Obstacle(770, 490, "img/kek.png", 0.185))
-level4.push(new Obstacle(770, 680, "img/sarga.png", 0.185))
+level4.push(new Obstacle(750, 680, "img/sarga.png", 0.185))
 
 
 level4.push(new Obstacle(620, 315, "img/kek.png", 0.185))
 level4.push(new Obstacle(620, 230, "img/sarga.png", 0.185))
-level4.push(new Obstacle(770, 405, "img/kek.png", 0.185))
+level4.push(new Obstacle(620, 405, "img/kek.png", 0.185))
 level4.push(new Obstacle(620, 490, "img/kek.png", 0.185))
 level4.push(new Obstacle(620, 592, "img/sarga.png", 0.185))
-level4.push(new Obstacle(620, 680, "img/sarga.png", 0.185))
+level4.push(new Obstacle(605, 680, "img/sarga.png", 0.185))
 
 // Level 5
 
