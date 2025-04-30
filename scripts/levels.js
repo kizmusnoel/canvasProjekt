f88b60cdc0db4d5af0139166c6f1c691e51b0bd5
Kiss <kissmarcell1@students.jedlik.eu>
2025-04-30 08:15:26 +0200

    level3

--- scripts/levels.js
+++ scripts/levels.js
@@ -4,31 +4,46 @@ export const levels = []
 
 
 // carX, carY, parkingspotX, parkingspotY
-const level1 = [100, 100, 585, 390] 
-const level2 = [100, 100, 975, 475]
-const level3 = [100, 110, 1150, 470]
+const level1 = [100, 100, 605, 400] 
+const level2 = [100, 100, 995, 490]
+const level3 = [100, 110, 1170, 490]
 const level4 = [100, 110, 400, 400]
 const level5 = [100, 110, 400, 400]
 
 
 // Level 1
 
-level1.push(new Obstacle(590, 305, "img/kek.png", 0.185))
-level1.push(new Obstacle(580, 485, "img/rendor.png", 0.6))
+level1.push(new Obstacle(610, 315, "img/kek.png", 0.185))
+level1.push(new Obstacle(600, 495, "img/rendor.png", 0.6))
 
 
 
 // Level 2
-level2.push(new Obstacle(990, 305, "img/rendor.png", 0.6))
-level2.push(new Obstacle(740, 305, "img/sarga.png", 0.185))
-level2.push(new Obstacle(990, 567.5, "img/kek.png", 0.185))
-level2.push(new Obstacle(580, 480, "img/rendor.png", 0.6))
-level2.push(new Obstacle(1150, 480, "img/kek.png", 0.185))
+level2.push(new Obstacle(1010, 315, "img/rendor.png", 0.6))
+level2.push(new Obstacle(760, 315, "img/sarga.png", 0.185))
+level2.push(new Obstacle(1010, 592, "img/kek.png", 0.185))
+level2.push(new Obstacle(590, 480, "img/rendor.png", 0.6))
+level2.push(new Obstacle(1180, 490, "img/kek.png", 0.185))
 
 
 
 // Level 3
+level3.push(new Obstacle(1150, 592, "img/kek.png", 0.185))
+level3.push(new Obstacle(1150, 315, "img/sarga.png", 0.185))
+level3.push(new Obstacle(1150, 230, "img/rendor.png", 0.6))
+level3.push(new Obstacle(1150, 405, "img/kek.png", 0.185))
 
+level3.push(new Obstacle(990, 592, "img/rendor.png", 0.6))
+level3.push(new Obstacle(1000, 315, "img/kek.png", 0.185))
+level3.push(new Obstacle(1000, 230, "img/sarga.png", 0.185))
+level3.push(new Obstacle(1000, 405, "img/sarga.png", 0.185))
+level3.push(new Obstacle(1000, 490, "img/kek.png", 0.185))
+
+
+level3.push(new Obstacle(1420, 315, "img/kek.png", 0.185))
+level3.push(new Obstacle(1420, 230, "img/sarga.png", 0.185))
+level3.push(new Obstacle(1420, 405, "img/sarga.png", 0.185))
+level3.push(new Obstacle(1420, 490, "img/kek.png", 0.185))
 
 
 // Level 4
